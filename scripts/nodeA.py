21272d69a363e9a19978916d50802fa537ab87f7
root <ceccorac@gmail.com>
2024-01-06 13:13:17 +0000

    Implemented nodeA, started nodeB

--- scripts/nodeA.py
+++ scripts/nodeA.py
@@ -1,99 +1,118 @@
 #! /usr/bin/env python
-import sys
-import select
 import rospy
 import actionlib
 import actionlib.msg
-import math
-import time
 import assignment_2_2023.msg
-
-from assignment_2_2023.msg import pos_vel
-from geometry_msgs.msg import Point, Pose, Twist
 from nav_msgs.msg import Odometry
-from std_srvs.srv import *
+from std_srvs.srv import SetBool
+from assignment_2_2023.msg import pos_vel
+from actionlib_msgs.msg import GoalStatus
+from geometry_msgs.msg import Point, Pose, Twist
+from assignment_2_2023.msg import PlanningAction, PlanningGoal, PlanningResult
 
 
+# Global variables
+publisher= None
+GoalCancelled = True
+
+# Publisher function
 def callback(msg):
-
 	global publisher
-
+	# Extract current position and velocity from the message
 	pos = msg.pose.pose.position
-	
 	vel = msg.twist.twist.linear
-	
-	msg_new = pos_vel()
-	
-	msg_new.x = position.x
-	
-	msg_new.y = position.y
-	
-	msg_new.vel_x = velocity.x
-	
-	msg_new.vel_z = velocity.z
-	
-	publisher.publish(msg_new)	
-		
-	
-	
-def client_request():
+	ang = msg.twist.twist.angular
+	# Create a new Vel message
+	posvel = pos_vel()
+	posvel.x = pos.x
+	posvel.y = pos.y
+	posvel.vel_x = vel.x
+	posvel.vel_z = ang.z
+	# Publish the message
+	publisher.publish(posvel)
 
+# Client function
+def client_request():
+	global x
+	global y
+	
+	# Create a SimpleActionClient
 	client = actionlib.SimpleActionClient('/reaching_goal', assignment_2_2023.msg.PlanningAction)
 	client.wait_for_server()
-	
+
+	# Loop until ROS is shutdown
 	while not rospy.is_shutdown():
 		
+		# Get the current goal position
+		x_param = rospy.get_param('/des_pos_x')
+		y_param = rospy.get_param('/des_pos_y')
+		# Create a new PlanningGoal
+		target = assignment_2_2023.msg.PlanningGoal()
+		target.target_pose.pose.position.x = x_param
+		target.target_pose.pose.position.y = y_param
+		rospy.loginfo("Current goal: target_x = %f, target_y = %f", x_param, y_param)
+		
+		
+		print("Set the coordinates of the goal")
+		# If the user wants to set a new goal
+		
 		try:
-			x_des = float(input("Set x coordinate: "))
-			y_des = float(input("Set y coordinate: "))
-		except:
-			print("Invalid coordinates")
+			x = float(input("Enter the x-coordinate for the new goal: "))
+			y = float(input("Enter the y-coordinate for the new goal: "))
+				
+			if (x > 9.0 or y > 9.0):
+				print("Please enter a valid input \nInput values must be between -8.0 and 8.0")
+				continue
+		except ValueError:
+			
+			rospy.logwarn("Invalid input. Please enter a valid number.")
 			continue
 			
-		target = assignment_2_2023.msg.PlanningGoal()
+		# Set the new goal position
+		rospy.set_param('/des_pos_x', x)
+		rospy.set_param('/des_pos_y', y)
 			
-		target.target_pose.pose.position.x = x_des
-		target.target_pose.pose.position.y = y_des
+		target.target_pose.pose.position.x = x
 			
-		client.send_goal(target, None, None)
-		
-		target_reached = False
-		
-		print("\nEnter 'c' to cancel the goal: ", end="")
-		
-		while not target_reached:
-		
-			tof = select.select([sys.stdin], [], [], 1)[0]
+		target.target_pose.pose.position.y = y
 			
-			if tof:
+		# Send the new goal
+		client.send_goal(target)
+		
 			
-				value = sys.stdin.readline().rstrip()
+		command = input("Insert 'c' if you want to cancel the goal otherwise type 'n' to insert a new goal: ")
+		# If the user wants to cancel the current goal
+		if command == 'c':
+		
+			client.cancel_goal()
+			rospy.loginfo("Current goal has been cancelled")
+			
+		elif command == 'n':
+			print("Set a new goal")
+			continue
 				
-				if value == 'c':
-				
-					print("Goal cancelled")
-					client.cancel_goal()
-					target_reached = True
-	
-	
-	
+		else:
+			rospy.logwarn("Invalid command. Please enter 'y' to insert a new goal or 'c' to cancel the current goal.")
+			
+		rospy.loginfo("Last received goal: target_x = %f, target_y = %f", target.target_pose.pose.position.x, target.target_pose.pose.position.y)
+
+# Main function
 def main():
 
+	# Initialize the node
 	rospy.init_node('nodeA')
 	
 	global publisher
-	publisher = rospy.Publisher("/PosVel", PosVel, queue_size = 1)
 	
-	rospy.Subscriber('/odom', Odometry, callback) 
+	# Create a publisher
+	publisher = rospy.Publisher("/pos_vel", pos_vel, queue_size=1)
 	
-	global client
+	# Subscribe to the /odom topic
+	rospy.Subscriber("/odom", Odometry, callback)
 	
+	# Run the client function
 	client_request()
-	
-		
-if __name__ == ' __main__':
 
+# Entry point
+if __name__ == '__main__':
 	main()
-
-		
-		 
---
+++ scripts/nodeB.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+import rospy
+import actionlib
+import actionlib.msg
+import assignment_2_2023.msg
+from nav_msgs.msg import Odometry
+from std_srvs.srv import SetBool
+from assignment_2_2023.msg import pos_vel
+from actionlib_msgs.msg import GoalStatus
+from geometry_msgs.msg import Point, Pose, Twist
+from assignment_2_2023.msg import PlanningAction, PlanningGoal, PlanningResult
+
+
+def main():
+
+	# Initialize the node
+	rospy.init_node('nodeB')
+	
+	global publisher
+	
+	# Create a publisher
+	publisher = rospy.Publisher("/pos_vel", pos_vel, queue_size=1)
+	
+	# Subscribe to the /odom topic
+	rospy.Subscriber("/odom", Odometry, callback)
+	
+	# Run the client function
+	client_request()
+
+# Entry point
+if __name__ == '__main__':
+	main()
